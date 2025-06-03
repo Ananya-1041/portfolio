9f3819b19ea9f48c16019badf76d8be26674ac1f
Ananya-1041 <ananya.edurkala@gmail.com>
2025-06-03 12:24:51 +0530

    my initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Achievements from './components/Achievements';
+import Hobbies from './components/Hobbies';
+import Contact from './components/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <Hero />
+                <About />
+                <Education />
+                <Skills />
+                <Projects />
+                <Achievements />
+                <Hobbies />
+                <Contact />
+            </main>
+        </div>
+    );
 }
 
 export default App;
